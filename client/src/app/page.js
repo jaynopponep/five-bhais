0bf386b34747130ec967304d9e59596dfadc79b5
Abdul-Andha <aandha8342@gmail.com>
2024-04-19 23:07:27 -0400

    landing page work for demo

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,12 +1,13 @@
 import Navbar from "./_components/navbar";
 import foodPic from "../../public/food.png"
 import Image from "next/image"
+import ReviewCard from "./_components/reviewCard";
 
 export default function Landing() {
   return (
     <div>
       <Navbar />
-      <div className="bg-customLight h-screen">
+      <div className="bg-customLight h-full pb-10 overflow-hidden">
         <div className="h-[450px] flex items-center justify-center">
           <div className="absolute z-50 text-6xl text-customLight text-center flex flex-col items-center justify-center">
             <div>Experience The <span className="text-customOrange">Flavors</span></div>
@@ -17,8 +18,15 @@ export default function Landing() {
             <div className="bg-customBlack/60 w-screen h-[450px] z-10 absolute"></div>
             <Image layout="fill" src={foodPic} alt="food" />
           </div>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <div className="text-5xl text-center font-bold mt-12 mb-6">Reviews</div>
+          <div className="flex justify-between w-11/12">
+            <ReviewCard />
+            <ReviewCard />
+            <ReviewCard />
 
-
+          </div>
         </div>
       </div>
     </div>
