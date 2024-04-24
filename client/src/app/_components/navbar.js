65542c839c3833a9dadc629a3cf7ae96ce31d083
Abdul-Andha <aandha8342@gmail.com>
2024-04-23 21:30:12 -0400

    signup and login frontend done. installed shadcn for components

--- client/src/app/_components/navbar.js
+++ client/src/app/_components/navbar.js
@@ -1,17 +1,18 @@
 import { LogIn } from 'lucide-react';
+import Link from 'next/link'
 
 export default function Navbar() {
   return (
     <>
       <div className="bg-customBrown text-customLight flex justify-between items-center p-4">
-        <div className="text-5xl font-extrabold">Bhai Brothers</div>
+        <Link href="/" className="text-5xl font-extrabold">Bhai Brothers</Link>
         <div className="text-2xl flex justify-around w-80">
           <div>Menu</div>
           <div>About</div>
-          <div className="font-extrabold flex items-center justify-between w-[105px]">
+          <Link href="login" className="font-extrabold flex items-center justify-between w-[105px]">
             <LogIn />
             <div>Sign In</div>
-          </div>
+          </Link>
         </div>
 
       </div>
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,6 +1,7 @@
 import Navbar from "./_components/navbar";
 import foodPic from "../../public/food.png"
 import Image from "next/image"
+import Link from "next/link";
 import ReviewCard from "./_components/reviewCard";
 
 export default function Landing() {
@@ -12,11 +13,13 @@ export default function Landing() {
           <div className="absolute z-50 text-6xl text-customLight text-center flex flex-col items-center justify-center">
             <div>Experience The <span className="text-customOrange">Flavors</span></div>
             <div>of <span className="text-customOrange">India</span> in <span className="text-customOrange">NYC</span></div>
-            <div className="bg-customOrange text-3xl font-bold w-56 h-12 rounded-lg flex items-center justify-center mt-16">Get Started</div>
+            <div className="bg-customOrange text-3xl font-bold w-56 h-12 rounded-lg flex items-center justify-center mt-16">
+              <Link href="signup">Get Started</Link>
+            </div>
           </div>
           <div className="h-[450px] w-screen relative">
             <div className="bg-customBlack/60 w-screen h-[450px] z-10 absolute"></div>
-            <Image layout="fill" src={foodPic} alt="food" />
+            <Image layout="fill" src={foodPic} alt="food" objectFit="cover" />
           </div>
         </div>
         <div className="flex flex-col justify-center items-center">
