d0cdb66b49df826cb797a53808c7863997fed35c
jaynopponep <noppone.p@gmail.com>
2024-05-05 09:12:26 -0400

    edited frontend error handle

--- client/src/app/signup/actions.js
+++ client/src/app/signup/actions.js
@@ -1,16 +1,24 @@
 "use server";
 
 export default async function signUp(formData) {
-  const response = await fetch('http://localhost:8080/api/v1/users/register', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(formData)
-  });
-  if (!response.ok) {
-    throw new Error('Failed to register');
+  try {
+    const response = await fetch('http://localhost:8080/api/v1/users/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    });
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      console.error('Registration failed: ', errorResponse.message);
+      throw new Error(errorResponse.message || 'Registration failed');
+    }
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error('Error occurred during registration:', error);
+    throw error;
   }
-  const data = await response.json();
-  console.log(data)
 }
