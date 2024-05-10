89088639a76f9ccf7cabb67d07937af503f54438
jaynopponep <noppone.p@gmail.com>
2024-05-10 15:25:03 -0400

    new: working checkout button

--- client/src/app/menu/components/customerMenu.js
+++ client/src/app/menu/components/customerMenu.js
@@ -1,9 +1,9 @@
-"use client"
-import Navbar from "../../_components/navbar";
+"use client";
+import NavbarLogged from "../../_components/navbar_loggedin";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import MenuSection from "./menuSection";
-import { fetchMenu } from "../actions"
+import { fetchMenu } from "../actions";
 
 export default function SurferMenu() {
   const [menu, setMenu] = useState([]);
@@ -16,15 +16,20 @@ export default function SurferMenu() {
 
   return (
     <div className="bg-customLight">
-      <Navbar />
+      <NavbarLogged />
       <div className="flex flex-col justify-center items-center text-customBlack pb-16">
         <div className="text-6xl font-bold mt-20 mb-4">Menu</div>
         <div className="border border-customBrown w-10/12 px-16 p-4">
           {menu.map((section, idx) => (
-            <MenuSection key={idx} title={section.sectionTitle} items={section.items} userType={"customer"} />
+            <MenuSection
+              key={idx}
+              title={section.sectionTitle}
+              items={section.items}
+              userType={"customer"}
+            />
           ))}
         </div>
       </div>
-    </div >
+    </div>
   );
 }
