fc383c1b516b802684ff8c31db061edd1b57588b
jaynopponep <noppone.p@gmail.com>
2024-07-25 21:39:19 -0400

    fixed inability to view cart

--- client/src/app/checkout/page.js
+++ client/src/app/checkout/page.js
@@ -1,6 +1,6 @@
 "use client";
 import Navbar from "../_components/navbar";
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Alert, AlertTitle } from "@/components/ui/alert";
 import Cart from "./components/cart";
 import CheckoutBox from "./components/checkoutBox";
@@ -26,35 +26,26 @@ export default function Checkout() {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    setCart(JSON.parse(localStorage.getItem("cart")));
+    const storedCart = JSON.parse(localStorage.getItem("cart")) || [];
+    setCart(storedCart);
     fetchUser()
-      .then((user) => {
-        setUser(user);
-        if (user.role === "surfer") {
-          router.push("/");
-        } else {
-          setIsLoading(false);
-        }
-      })
-      .catch((error) => console.error(error));
-  }, []);
+        .then((user) => {
+          setUser(user);
+          if (user.role === "surfer") {
+            router.push("/");
+          } else {
+            setIsLoading(false);
+          }
+        })
+        .catch((error) => console.error(error));
+  }, [router]);
 
-  const [userType, setUserType] = useState("");
   useEffect(() => {
     fetchUser()
-      .then((data) => {
-        setUserType(data.role);
-      })
-      .catch((error) => console.error(error));
-  }, []);
-
-  const [userEmail, setUserEmail] = useState("");
-  useEffect(() => {
-    fetchUser()
-      .then((data) => {
-        setUserEmail(data.email);
-      })
-      .catch((error) => console.error(error));
+        .then((data) => {
+          setUser(data);
+        })
+        .catch((error) => console.error(error));
   }, []);
 
   const handleInputChange = (e) => {
@@ -98,7 +89,7 @@ export default function Checkout() {
   };
 
   const updateQuantity = (itemID, amount) => {
-    let cart = JSON.parse(localStorage.getItem("cart"));
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
     for (const cartItem of cart) {
       if (cartItem._id.$oid === itemID) {
         if (cartItem.quantity + amount < 0) return;
@@ -114,11 +105,11 @@ export default function Checkout() {
     e.preventDefault();
     setErrorMsg("");
     if (
-      formData.orderType === "delivery" &&
-      (formData.address === "" ||
-        formData.city === "" ||
-        formData.state === "" ||
-        formData.zip === "")
+        formData.orderType === "delivery" &&
+        (formData.address === "" ||
+            formData.city === "" ||
+            formData.state === "" ||
+            formData.zip === "")
     ) {
       setErrorMsg("Please fill out all fields.");
       return;
@@ -128,16 +119,15 @@ export default function Checkout() {
       return;
     }
     try {
-      const total = userType === "vipcustomer" ? discountedTotal() : getTotal();
-      const data = await placeOrder(cart, formData, userEmail, total);
+      const total = user.role === "vipcustomer" ? discountedTotal() : getTotal();
+      const data = await placeOrder(cart, formData, user.email, total);
       if (data) {
-        // Add cart^^
         localStorage.setItem("cart", JSON.stringify([]));
         setCart([]);
         setErrorMsg("");
-        const user = JSON.parse(sessionStorage.getItem("user"));
-        user.balance = user.balance - total;
-        sessionStorage.setItem("user", JSON.stringify(user));
+        const updatedUser = { ...user, balance: user.balance - total };
+        sessionStorage.setItem("user", JSON.stringify(updatedUser));
+        setUser(updatedUser);
         setSuccessMsg("Order placed successfully!");
       }
     } catch (error) {
@@ -147,49 +137,50 @@ export default function Checkout() {
 
   if (isLoading) return <Loading />;
   return (
-    <div className="bg-customLight pb-10 min-h-screen">
-      <Navbar />
-      <div className="mt-20 ml-12 flex justify-around">
-        {errorMsg ? (
-          <Alert
-            variant="destructive"
-            className="w-[250px] text-center absolute top-24"
-          >
-            <AlertTitle>{errorMsg}</AlertTitle>
-          </Alert>
-        ) : null}
-        {successMsg ? (
-          <Alert
-            variant="default"
-            className="w-[250px] text-center absolute top-24"
-          >
-            <AlertTitle>{successMsg}</AlertTitle>
-          </Alert>
-        ) : null}
-        <div className="w-7/12">
-          <div className="text-4xl font-bold mb-6">Your Cart</div>
-          {cart.length === 0 ? (
-            <div className="text-4xl text-gray-500 text-center mt-56">
-              Your cart is empty.
-            </div>
-          ) : (
-            <Cart cart={cart} updateQuantity={updateQuantity} />
+      <div className="bg-customLight pb-10 min-h-screen">
+        <Navbar />
+        <div className="mt-20 ml-12 flex justify-around">
+          {errorMsg ? (
+              <Alert
+                  variant="destructive"
+                  className="w-[250px] text-center absolute top-24"
+              >
+                <AlertTitle>{errorMsg}</AlertTitle>
+              </Alert>
+          ) : null}
+          {successMsg ? (
+              <Alert
+                  variant="default"
+                  className="w-[250px] text-center absolute top-24"
+              >
+                <AlertTitle>{successMsg}</AlertTitle>
+              </Alert>
+          ) : null}
+          <div className="w-7/12">
+            <div className="text-4xl font-bold mb-6">Your Cart</div>
+            {cart.length === 0 ? (
+                <div className="text-4xl text-gray-500 text-center mt-56">
+                  Your cart is empty.
+                </div>
+            ) : (
+                <Cart cart={cart} updateQuantity={updateQuantity} />
+            )}
+          </div>
+          {cart.length !== 0 && (
+              <CheckoutBox
+                  formData={formData}
+                  handleInputChange={handleInputChange}
+                  handleSubmit={handleSubmit}
+                  toggleOrderType={toggleOrderType}
+                  getSubTotal={getSubTotal}
+                  getDiscount={getDiscount}
+                  getTax={getTax}
+                  getTotal={getTotal}
+                  discountedTotal={discountedTotal}
+              />
           )}
         </div>
-        {cart.length != 0 ? (
-          <CheckoutBox
-            formData={formData}
-            handleInputChange={handleInputChange}
-            handleSubmit={handleSubmit}
-            toggleOrderType={toggleOrderType}
-            getSubTotal={getSubTotal}
-            getDiscount={getDiscount}
-            getTax={getTax}
-            getTotal={getTotal}
-            discountedTotal={discountedTotal}
-          />
-        ) : null}
       </div>
-    </div>
   );
 }
+
